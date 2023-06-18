1f1984d6e2c36ca705a2d62e80fd265270666a9b
Ronny Granados <ronnygranados7@gmail.com>
2023-06-18 04:54:20 -0600

    Creación de Estados LOSS of Sync y Comma Dettect 1

--- synch.v
+++ synch.v
@@ -13,12 +13,18 @@ module synchronization(
                        output reg GOOD_CGS
 );
 
+localparam comma1 = 6'b110000;
+localparam comma2 = 6'b001111;
+
 // -----------------------------------------------
 
 /* Según el diagrama del standart, 
    la sincronización cuenta con 13 estados 
    (Figura 36-9)
 */
+
+
+parameter IDLE = 0;
 parameter LOSS_OF_SYNC = 1;
 parameter COMMA_DETECT_1 = 2;
 parameter ACCQUIRE_SYNC_1 = 3;
@@ -35,18 +41,35 @@ parameter SYNC_ACQUIRED_2A = 11;
 parameter SYNC_ACQUIRED_3A = 12;
 parameter SYNC_ACQUIRED_4A = 13;
 
-parameter estado = LOSS_OF_SYNC; // Estado inicial
+integer estado = IDLE; // Estado inicial
 
 // -----------------------------------------------
 
 always @(negedge CLK) begin
-    /*if (POWER || RESET || (SIGNAL_CHANGE && ~MR_LOOPBACK && PUDI)) begin
-        $display("Full detection of signals");
-    end*/
 
-    if (POWER) begin
-        $display("Full detection of signals");
-    end
+    case (estado)
+        IDLE: begin
+          if (POWER==1'b1 || RESET==1'b1 || (SIGNAL_CHANGE==1'b1 && MR_LOOPBACK==1'b0 && PUDI)) begin
+            $display("Next State: LOSS_OF_SYNC");
+            estado = estado + 1;
+          end
+        end
+
+        LOSS_OF_SYNC: begin
+            CODE_SYNC <= 0;
+            RX_EVEN <= !RX_EVEN;
+            SUDI[9:0] = PUDI[9:0];
+
+            if ((PUDI && SIGNAL_DETECT==1'b0 && MR_LOOPBACK==1'b0) || PUDI[9:4]!=comma1) begin
+                estado = LOSS_OF_SYNC;
+            end
+
+            if ((SIGNAL_DETECT==1 || MR_LOOPBACK==1) && PUDI[9:4]==comma1) begin
+                estado = COMMA_DETECT_1
+            end
+            $display("Current State: LOSS_OF_SYNC");
+        end
+    endcase
 end
 
 endmodule
